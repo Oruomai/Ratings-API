58753fb4251eb77e7ade714fba34beb3cc9e3ccc
David Moon <hrmoon99@gmail.com>
2022-11-06 03:19:48 -0600

    1.0.0

---
+++ index.js
@@ -0,0 +1,26 @@
+import express from 'express'
+import cors from 'cors'
+import 'dotenv/config'
+import {MongoClient} from 'mongodb'
+
+const URI = process.env.MONGO_URI
+const client = new MongoClient(URI)
+const database = client.db('Database')
+const games = database.collection('games')
+const MLOutputAVG = database.collection('MLOutputAVG')
+
+const PORT = process.env.PORT
+
+client.connect()
+console.log('connected to mongodb')
+
+const app = express()
+app.use(cors())
+app.use(express.json())
+
+app.listen(4444, () => console.log('api running'))
+
+app.get('/', async (req, res) => {
+    const Ratings = await MLOutputAVG.find().toArray()
+    res.json(Ratings)
+})
